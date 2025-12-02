abb4df3cf94cdc223474ae9187d524d7815531a5
Lukas Bajer <lukasb@kentico.com>
2025-12-02 10:54:02 +0100

    Improve translation prompt to remove/fix non valid HTML tags, adds schema definition config.

--- admin-localization-ai-translator/src/server.ts
+++ admin-localization-ai-translator/src/server.ts
@@ -19,6 +19,7 @@ interface ResourceEntry {
   value: string;
   comment?: string;
   xmlSpace?: string;
+  schemaDef?: string;
 }
 
 // Parse .resx XML file
@@ -104,7 +105,7 @@ Translated resources:`;
       messages: [
         {
           role: "system",
-          content: `You are a professional translator specializing in software localization. Translate resource values to ${targetLanguage} while preserving all technical formatting.`
+          content: `You are a professional translator specializing in software localization. Translate resource values to ${targetLanguage} while preserving all technical formatting. Ensure the output contains valid xml code. CRITICAL: remove any non-valid xml and html tags, namely <lt;/a>, <lt;/bgt, <brgt;, <bgt;, "&lt;b&gt;, &lt;b&gt;, multiple <br><br> tags, or <br > </ br> tags.`
         },
         {
           role: "user",
@@ -145,6 +146,66 @@ Translated resources:`;
   }
 }
 
+const schemaDef = `<xsd:schema id="root" xmlns="" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:msdata="urn:schemas-microsoft-com:xml-msdata">
+    <xsd:import namespace="http://www.w3.org/XML/1998/namespace" />
+    <xsd:element name="root" msdata:IsDataSet="true">
+      <xsd:complexType>
+        <xsd:choice maxOccurs="unbounded">
+          <xsd:element name="metadata">
+            <xsd:complexType>
+              <xsd:sequence>
+                <xsd:element name="value" type="xsd:string" minOccurs="0" />
+              </xsd:sequence>
+              <xsd:attribute name="name" use="required" type="xsd:string" />
+              <xsd:attribute name="type" type="xsd:string" />
+              <xsd:attribute name="mimetype" type="xsd:string" />
+              <xsd:attribute ref="xml:space" />
+            </xsd:complexType>
+          </xsd:element>
+          <xsd:element name="assembly">
+            <xsd:complexType>
+              <xsd:attribute name="alias" type="xsd:string" />
+              <xsd:attribute name="name" type="xsd:string" />
+            </xsd:complexType>
+          </xsd:element>
+          <xsd:element name="data">
+            <xsd:complexType>
+              <xsd:sequence>
+                <xsd:element name="value" type="xsd:string" minOccurs="0" msdata:Ordinal="1" />
+                <xsd:element name="comment" type="xsd:string" minOccurs="0" msdata:Ordinal="2" />
+              </xsd:sequence>
+              <xsd:attribute name="name" type="xsd:string" use="required" msdata:Ordinal="1" />
+              <xsd:attribute name="type" type="xsd:string" msdata:Ordinal="3" />
+              <xsd:attribute name="mimetype" type="xsd:string" msdata:Ordinal="4" />
+              <xsd:attribute ref="xml:space" />
+            </xsd:complexType>
+          </xsd:element>
+          <xsd:element name="resheader">
+            <xsd:complexType>
+              <xsd:sequence>
+                <xsd:element name="value" type="xsd:string" minOccurs="0" msdata:Ordinal="1" />
+              </xsd:sequence>
+              <xsd:attribute name="name" type="xsd:string" use="required" />
+            </xsd:complexType>
+          </xsd:element>
+        </xsd:choice>
+      </xsd:complexType>
+    </xsd:element>
+  </xsd:schema>
+  <resheader name="resmimetype">
+    <value>text/microsoft-resx</value>
+  </resheader>
+  <resheader name="version">
+    <value>2.0</value>
+  </resheader>
+  <resheader name="reader">
+    <value>System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+  <resheader name="writer">
+    <value>System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </resheader>
+`;  
+
 // Format entries as .resx XML
 function formatResourceFile(entries: ResourceEntry[], header: string, footer: string): string {
   const dataElements = entries.map(entry => {
@@ -156,7 +217,7 @@ function formatResourceFile(entries: ResourceEntry[], header: string, footer: st
   </data>`;
   });
   
-  return header + dataElements.join('\n') + footer;
+  return header + schemaDef + dataElements.join('\n') + footer;
 }
 
 
